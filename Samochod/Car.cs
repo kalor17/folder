3e9f35edc55ac973e3168f98d73a13b97ac33f3f
kalor17 <wisniak17@gmail.com>
2016-10-17 15:42:48 +0200

    pierwszt commit

---
+++ Samochod/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samochod
+{
+    class Car
+    {
+        public double pojSilnika;
+        public string marka;
+        public static int ilKol;
+
+        static Car()
+        {
+            ilKol = 4;
+        }
+
+        public Car(double pojSilnika, string marka)
+        {
+            this.pojSilnika = pojSilnika;
+            this.marka = marka;
+        }
+
+        public static Car Create(double pojSilnika,string marka)
+        {
+            return new Car(pojSilnika,marka);
+        }
+
+        ~Car()
+        {
+            Console.WriteLine("Zwalniam pamięć");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Samochod/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samochod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car zbyszek = Car.Create(2.0, "zbychu");
+            Console.WriteLine(zbyszek.marka);
+            
+        }
+    }
+}
